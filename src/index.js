526f17ed7c80b30f6eaae290151d16ee15759ae6
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2024-09-27 16:47:21 +0300

    Complete lab task, select Midpoint Rule for impl

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("Let's integrate!")
+function MidpointRule(x) {
+    return 4 * x - x * x;
+}
+
+let step = 1;
+
+function Integration(minX, maxX, F) {
+    var summ = 0;
+    for (let x = minX; x < maxX; x += step) {
+        summ += F(x);
+    }
+    return summ;
+}
+
+let minX = Number(prompt('Input minX:', 0)); 
+let maxX = Number(prompt('Input maxX:', 0)); 
+let integrationX = Integration(minX, maxX, MidpointRule);
+alert('Function: ' + integrationX);
